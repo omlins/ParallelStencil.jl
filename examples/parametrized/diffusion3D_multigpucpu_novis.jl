453c89e5d350792564e6e1b14cbf2ffbbd4a38ff
Samuel Omlin <samuel.omlin@cscs.ch>
2022-02-24 11:26:45 +0100

    Update diffusion3D_multigpucpu_novis.jl

--- examples/parametrized/diffusion3D_multigpucpu_novis.jl
+++ examples/parametrized/diffusion3D_multigpucpu_novis.jl
@@ -14,44 +14,43 @@ end
 end
 
 function diffusion3D(lam::_T, c0::_T, lx::_T, ly::_T, lz::_T) where _T <: PSNumber
+    # Numerics
+    nx, ny, nz = 512, 512, 512;                              # Number of gridpoints in dimensions x, y and z
+    nt         = 100;                                        # Number of time steps
+    me, dims   = init_global_grid(nx, ny, nz);
+    dx         = lx/(nx_g()-1);                              # Space step in x-dimension
+    dy         = ly/(ny_g()-1);                              # Space step in y-dimension
+    dz         = lz/(nz_g()-1);                              # Space step in z-dimension
+    _dx, _dy, _dz = 1.0/dx, 1.0/dy, 1.0/dz;
 
-# Numerics
-nx, ny, nz = 512, 512, 512;                              # Number of gridpoints in dimensions x, y and z
-nt         = 100;                                        # Number of time steps
-me, dims   = init_global_grid(nx, ny, nz);
-dx         = lx/(nx_g()-1);                              # Space step in x-dimension
-dy         = ly/(ny_g()-1);                              # Space step in y-dimension
-dz         = lz/(nz_g()-1);                              # Space step in z-dimension
-_dx, _dy, _dz = 1.0/dx, 1.0/dy, 1.0/dz;
+    # Array initializations
+    T   = @zeros(_T, nx, ny, nz);
+    T2  = @zeros(_T, nx, ny, nz);
+    Ci  = @zeros(_T, nx, ny, nz);
 
-# Array initializations
-T   = @zeros(_T, nx, ny, nz);
-T2  = @zeros(_T, nx, ny, nz);
-Ci  = @zeros(_T, nx, ny, nz);
+    # Initial conditions
+    Ci .= 1/c0;                                              # 1/Heat capacity
+    T  .= 1.7;
+    T2 .= T;                                                 # Assign also T2 to get correct boundary conditions.
 
-# Initial conditions
-Ci .= 1/c0;                                              # 1/Heat capacity
-T  .= 1.7;
-T2 .= T;                                                 # Assign also T2 to get correct boundary conditions.
+    # Time loop
+    dt   = min(dx^2,dy^2,dz^2)/lam/maximum(Ci)/8.1;          # Time step for 3D Heat diffusion
+    lam, dt, _dx, _dy, _dz = _T.((lam, dt, _dx, _dy, _dz))   # Convert all scalars to the required numbertype.
+    for it = 1:nt
+        if (it == 11) tic(); end                             # Start measuring time.
+        @parallel diffusion3D_step!(T2, T, Ci, lam, dt, _dx, _dy, _dz);
+        update_halo!(T2);
+        T, T2 = T2, T;
+    end
+    time_s = toc()
 
-# Time loop
-dt   = min(dx^2,dy^2,dz^2)/lam/maximum(Ci)/8.1;          # Time step for 3D Heat diffusion
-lam, dt, _dx, _dy, _dz = _T.((lam, dt, _dx, _dy, _dz))   # Convert all scalars to the required numbertype.
-for it = 1:nt
-    if (it == 11) tic(); end                             # Start measuring time.
-    @parallel diffusion3D_step!(T2, T, Ci, lam, dt, _dx, _dy, _dz);
-    update_halo!(T2);
-    T, T2 = T2, T;
-end
-time_s = toc()
+    # Performance
+    A_eff = (2*1+1)*1/1e9*nx*ny*nz*sizeof(_T);               # Effective main memory access per iteration [GB] (Lower bound of required memory access: T has to be read and written: 2 whole-array memaccess; Ci has to be read: : 1 whole-array memaccess)
+    t_it  = time_s/(nt-10);                                  # Execution time per iteration [s]
+    T_eff = A_eff/t_it;                                      # Effective memory throughput [GB/s]
+    if (me==0) println("time_s=$time_s T_eff=$T_eff"); end
 
-# Performance
-A_eff = (2*1+1)*1/1e9*nx*ny*nz*sizeof(_T);               # Effective main memory access per iteration [GB] (Lower bound of required memory access: T has to be read and written: 2 whole-array memaccess; Ci has to be read: : 1 whole-array memaccess)
-t_it  = time_s/(nt-10);                                  # Execution time per iteration [s]
-T_eff = A_eff/t_it;                                      # Effective memory throughput [GB/s]
-if (me==0) println("time_s=$time_s T_eff=$T_eff"); end
-
-finalize_global_grid();
+    finalize_global_grid();
 end
 
 
