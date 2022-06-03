921ea420135fa6a21c2677daa5f02da52120e096
Samuel Omlin <samuel.omlin@cscs.ch>
2022-06-03 20:14:24 +0200

    add @CellType macro

--- src/ParallelKernel/ParallelKernel.jl
+++ src/ParallelKernel/ParallelKernel.jl
@@ -57,7 +57,7 @@ include("parallel.jl")
 include("reset_parallel_kernel.jl")
 
 ## Exports
-export @init_parallel_kernel, @parallel, @hide_communication, @parallel_indices, @parallel_async, @synchronize, @zeros, @ones, @rand, @falses, @trues, @fill, @fill!
+export @init_parallel_kernel, @parallel, @hide_communication, @parallel_indices, @parallel_async, @synchronize, @zeros, @ones, @rand, @falses, @trues, @fill, @fill!, @CellType
 export @gridDim, @blockIdx, @blockDim, @threadIdx, @sync_threads, @sharedMem, @pk_show, @pk_println
 export PKNumber
 
