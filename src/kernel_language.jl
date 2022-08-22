6029f6eb2b2cc4c332ac55910deadd31a3028fa5
Samuel Omlin <samuel.omlin@cscs.ch>
2022-08-22 12:15:16 +0200

    remove SHMEMHALO const from macros

--- src/kernel_language.jl
+++ src/kernel_language.jl
@@ -1,15 +1,15 @@
 #TODO: add ParallelStencil.ParallelKernel. in front of all kernel lang in macros! Later: generalize more for z?
 
 ##
-macro nx_l(args...) check_initialized(); checknoargs(args...); esc(nx_l(args...)); end
+macro nx_l(args...) check_initialized(); checksinglearg(args...); esc(nx_l(args...)); end
 
 
 ##
-macro ny_l(args...) check_initialized(); checknoargs(args...); esc(ny_l(args...)); end
+macro ny_l(args...) check_initialized(); checksinglearg(args...); esc(ny_l(args...)); end
 
 
 ##
-macro nz_l(args...) check_initialized(); checknoargs(args...); esc(nz_l(args...)); end
+macro nz_l(args...) check_initialized(); checksinglearg(args...); esc(nz_l(args...)); end
 
 
 ##
@@ -17,39 +17,39 @@ macro t_h(args...) check_initialized(); checknoargs(args...); esc(t_h(args...));
 
 
 ##
-macro t_h2(args...) check_initialized(); checknoargs(args...); esc(t_h2(args...)); end
+macro t_h2(args...) check_initialized(); checktwoargs(args...); esc(t_h2(args...)); end
 
 
 ##
-macro tx_h(args...) check_initialized(); checknoargs(args...); esc(tx_h(args...)); end
+macro tx_h(args...) check_initialized(); checksinglearg(args...); esc(tx_h(args...)); end
 
 
 ##
-macro ty_h(args...) check_initialized(); checknoargs(args...); esc(ty_h(args...)); end
+macro ty_h(args...) check_initialized(); checksinglearg(args...); esc(ty_h(args...)); end
 
 
 ##
-macro tx_h2(args...) check_initialized(); checknoargs(args...); esc(tx_h2(args...)); end
+macro tx_h2(args...) check_initialized(); checktwoargs(args...); esc(tx_h2(args...)); end
 
 
 ##
-macro ty_h2(args...) check_initialized(); checknoargs(args...); esc(ty_h2(args...)); end
+macro ty_h2(args...) check_initialized(); checktwoargs(args...); esc(ty_h2(args...)); end
 
 
 ##
-macro ix_h(args...) check_initialized(); checknoargs(args...); esc(ix_h(args...)); end
+macro ix_h(args...) check_initialized(); checksinglearg(args...); esc(ix_h(args...)); end
 
 
 ##
-macro iy_h(args...) check_initialized(); checknoargs(args...); esc(iy_h(args...)); end
+macro iy_h(args...) check_initialized(); checktwoargs(args...); esc(iy_h(args...)); end
 
 
 ##
-macro ix_h2(args...) check_initialized(); checknoargs(args...); esc(ix_h2(args...)); end
+macro ix_h2(args...) check_initialized(); checktwoargs(args...); esc(ix_h2(args...)); end
 
 
 ##
-macro iy_h2(args...) check_initialized(); checknoargs(args...); esc(iy_h2(args...)); end
+macro iy_h2(args...) check_initialized(); checktwoargs(args...); esc(iy_h2(args...)); end
 
 
 ##
@@ -66,6 +66,14 @@ function checknoargs(args...)
     if (length(args) != 0) @ArgumentError("no arguments allowed.") end
 end
 
+function checksinglearg(args...)
+    if (length(args) != 1) @ArgumentError("wrong number of arguments.") end
+end
+
+function checktwoargs(args...)
+    if (length(args) != 2) @ArgumentError("wrong number of arguments.") end
+end
+
 function checkargs_loop(args...)
     if (length(args) != 4) @ArgumentError("wrong number of arguments.") end
 end
@@ -77,19 +85,19 @@ end
 
 ## FUNCTIONS FOR INDEXING AND DIMENSIONS
 
-function nx_l(args...; package::Symbol=get_package())
+function nx_l(hx; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :(@blockDim().x + (2*SHMEM_HALO_X))
+    return :(@blockDim().x + (2*$hx))
 end
 
-function ny_l(args...; package::Symbol=get_package())
+function ny_l(hy; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :(@blockDim().y + (2*SHMEM_HALO_Y))
+    return :(@blockDim().y + (2*$hy))
 end
 
-function nz_l(args...; package::Symbol=get_package())
+function nz_l(hz; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :(@blockDim().z + (2*SHMEM_HALO_Z))
+    return :(@blockDim().z + (2*$hz))
 end
 
 function t_h(args...; package::Symbol=get_package())
@@ -97,49 +105,49 @@ function t_h(args...; package::Symbol=get_package())
     return :((@threadIdx().y-1)*@blockDim().x + @threadIdx().x)
 end
 
-function t_h2(args...; package::Symbol=get_package())
+function t_h2(hx, hy; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :(@t_h() + @nx_l()*@ny_l() - @blockDim().x*@blockDim().y)
+    return :(@t_h() + @nx_l($hx)*@ny_l($hy) - @blockDim().x*@blockDim().y)
 end
 
-function tx_h(args...; package::Symbol=get_package())
+function tx_h(hx; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :((@t_h() -1) % @nx_l() + 1)
+    return :((@t_h() -1) % @nx_l($hx) + 1)
 end
 
-function ty_h(args...; package::Symbol=get_package())
+function ty_h(hx; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :((@t_h() -1) ÷ @nx_l() + 1)
+    return :((@t_h() -1) ÷ @nx_l($hx) + 1)
 end
 
-function tx_h2(args...; package::Symbol=get_package())
+function tx_h2(hx, hy; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :((@t_h2()-1) % @nx_l() + 1)
+    return :((@t_h2($hx, $hy)-1) % @nx_l($hx) + 1)
 end
 
-function ty_h2(args...; package::Symbol=get_package())
+function ty_h2(hx, hy; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :((@t_h2()-1) ÷ @nx_l() + 1)
+    return :((@t_h2($hx, $hy)-1) ÷ @nx_l($hx) + 1)
 end
 
-function ix_h(args...; package::Symbol=get_package())
+function ix_h(hx; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :((@blockIdx().x-1)*@blockDim().x + @tx_h()  - SHMEM_HALO_X)
+    return :((@blockIdx().x-1)*@blockDim().x + @tx_h($hx)  - $hx)
 end
 
-function iy_h(args...; package::Symbol=get_package())
+function iy_h(hx, hy; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :((@blockIdx().y-1)*@blockDim().y + @ty_h()  - SHMEM_HALO_Y)
+    return :((@blockIdx().y-1)*@blockDim().y + @ty_h($hx)  - $hy)
 end
 
-function ix_h2(args...; package::Symbol=get_package())
+function ix_h2(hx, hy; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :((@blockIdx().x-1)*@blockDim().x + @tx_h2() - SHMEM_HALO_X)
+    return :((@blockIdx().x-1)*@blockDim().x + @tx_h2($hx, $hy) - $hx)
 end
 
-function iy_h2(args...; package::Symbol=get_package())
+function iy_h2(hx, hy; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
-    return :((@blockIdx().y-1)*@blockDim().y + @ty_h2() - SHMEM_HALO_Y)
+    return :((@blockIdx().y-1)*@blockDim().y + @ty_h2($hx, $hy) - $hy)
 end
 
 
@@ -161,12 +169,12 @@ end
 #TODO: see what to do with global consts as SHMEM_HALO_X,... Support later multiple vars for opt (now just A=T...)
 #TODO: add input check and errors
 #TODO: maybe gensym with macro @gensym
-function loopopt(dim::Integer, indices, loopsize, shmemhalo, A, body; package::Symbol=get_package())
+function loopopt(dim::Integer, indices, loopsize, halosize, A, body; package::Symbol=get_package())
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
     if isa(indices,Expr) indices = indices.args else indices = [indices] end
-    if isa(shmemhalo,Expr) shmemhalo = shmemhalo.args else shmemhalo = [shmemhalo] end
+    if isa(halosize,Expr) halosize = halosize.args else halosize = [halosize] end
     if dim == 3
-        shmemhalox, shmemhaloy = shmemhalo
+        hx, hy       = halosize
         ix, iy, iz   = indices
         i            = gensym_world("i", @__MODULE__)
         tx           = gensym_world("tx", @__MODULE__)
@@ -194,14 +202,14 @@ function loopopt(dim::Integer, indices, loopsize, shmemhalo, A, body; package::S
         body = substitute(body, :($A[$ix,$iy+1,$iz]), A_ix_iyp1_iz)
         
         return quote
-            $tx            = @threadIdx().x + $shmemhalox
-            $ty            = @threadIdx().y + $shmemhaloy
-            $ix_h          = @ix_h()  #(@blockIdx().x-1)*@blockDim().x + @tx_h()  - SHMEM_HALO_X
-            $ix_h2         = @ix_h2() #(@blockIdx().x-1)*@blockDim().x + @tx_h2() - SHMEM_HALO_X
-            $iy_h          = @iy_h()  #(@blockIdx().y-1)*@blockDim().y + @ty_h()  - SHMEM_HALO_Y
-            $iy_h2         = @iy_h2() #(@blockIdx().y-1)*@blockDim().y + @ty_h2() - SHMEM_HALO_Y
+            $tx            = @threadIdx().x + $hx
+            $ty            = @threadIdx().y + $hy
+            $ix_h          = @ix_h($hx)  #(@blockIdx().x-1)*@blockDim().x + @tx_h()  - SHMEM_HALO_X
+            $ix_h2         = @ix_h2($hx, $hy) #(@blockIdx().x-1)*@blockDim().x + @tx_h2() - SHMEM_HALO_X
+            $iy_h          = @iy_h($hx, $hy)  #(@blockIdx().y-1)*@blockDim().y + @ty_h()  - SHMEM_HALO_Y
+            $iy_h2         = @iy_h2($hx, $hy) #(@blockIdx().y-1)*@blockDim().y + @ty_h2() - SHMEM_HALO_Y
             $loopoffset    = (@blockIdx().z-1)*$loopsize #TODO: MOVE UP - see no perf change! interchange other lines!
-            $A_izp1        = @sharedMem(eltype($A), (@nx_l(), @ny_l()))
+            $A_izp1        = @sharedMem(eltype($A), (@nx_l($hx), @ny_l($hy)))
             $A_ix_iy_izm1  = 0.0
             $A_ix_iy_iz    = $A[$ix,$iy,1+$loopoffset]
             $A_ix_iy_izp1  = 0.0
@@ -212,11 +220,11 @@ function loopopt(dim::Integer, indices, loopsize, shmemhalo, A, body; package::S
             for $i = 1:$loopsize
                 $iz = $i + $loopoffset
                 @sync_threads()
-                if (@t_h() <= cld(@nx_l()*@ny_l(),2) && $ix_h>0 && $ix_h<=size($A,1) && $iy_h>0 && $iy_h<=size($A,2) && $iz<size($A,3)) 
-                    $A_izp1[@tx_h(),@ty_h()] = $A[$ix_h,$iy_h,$iz+1] 
+                if (@t_h() <= cld(@nx_l($hx)*@ny_l($hy),2) && $ix_h>0 && $ix_h<=size($A,1) && $iy_h>0 && $iy_h<=size($A,2) && $iz<size($A,3)) 
+                    $A_izp1[@tx_h($hx),@ty_h($hx)] = $A[$ix_h,$iy_h,$iz+1] 
                 end
-                if (@t_h2() > cld(@nx_l()*@ny_l(),2) && $ix_h2>0 && $ix_h2<=size($A,1) && $iy_h2>0 && $iy_h2<=size($A,2) && $iz<size($A,3)) 
-                    $A_izp1[@tx_h2(),@ty_h2()] = $A[$ix_h2,$iy_h2,$iz+1]
+                if (@t_h2($hx, $hy) > cld(@nx_l($hx)*@ny_l($hy),2) && $ix_h2>0 && $ix_h2<=size($A,1) && $iy_h2>0 && $iy_h2<=size($A,2) && $iz<size($A,3)) 
+                    $A_izp1[@tx_h2($hx, $hy),@ty_h2($hx, $hy)] = $A[$ix_h2,$iy_h2,$iz+1]
                 end
                 @sync_threads()
                 $A_ix_iy_izp1 = $A_izp1[$tx,$ty]
