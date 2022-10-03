a36e1b57e2996fc341bbdad7d0d5258ba7bc08f9
Samuel Omlin <samuel.omlin@cscs.ch>
2022-10-03 16:12:46 +0200

    make hide comm expand to generic PS macros rather than directly PK macros

--- src/ParallelKernel/hide_communication.jl
+++ src/ParallelKernel/hide_communication.jl
@@ -97,10 +97,10 @@ function hide_communication_cuda(ranges_outer::Union{Symbol,Expr}, ranges_inner:
     bc_and_commcalls = process_bc_and_commcalls(bc_and_commcalls)
     quote
         for i in 1:length($ranges_outer)
-            ParallelStencil.ParallelKernel.@parallel_async $ranges_outer[i] stream=ParallelStencil.ParallelKernel.get_priority_custream(i) $compkernelcall
+            @parallel_async $ranges_outer[i] stream=ParallelStencil.ParallelKernel.get_priority_custream(i) $(kwargs...) $compkernelcall #NOTE: it cannot directly go to ParallelStencil.ParallelKernel.@parallel_async as else it cannot honour ParallelStencil args as loopopt.
         end
         for i in 1:length($ranges_inner)
-            ParallelStencil.ParallelKernel.@parallel_async $ranges_inner[i] stream=ParallelStencil.ParallelKernel.get_custream(i) $compkernelcall
+            @parallel_async $ranges_inner[i] stream=ParallelStencil.ParallelKernel.get_custream(i) $(kwargs...) $compkernelcall          #NOTE: ...
         end
         for i in 1:length($ranges_outer) ParallelStencil.ParallelKernel.@synchronize(ParallelStencil.ParallelKernel.get_priority_custream(i)); end
         $bc_and_commcalls
