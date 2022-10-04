6e4a388f61ec126ef1c398bc8b4b4babff0e1d10
Samuel Omlin <samuel.omlin@cscs.ch>
2022-10-04 19:32:49 +0200

    test only if AMDGPU functional

--- test/test_incremental_compilation.jl
+++ test/test_incremental_compilation.jl
@@ -2,11 +2,15 @@ push!(LOAD_PATH, "@stdlib")  # NOTE: this is needed to enable this test to run f
 push!(LOAD_PATH, joinpath(@__DIR__, ".."))
 using Test
 using Pkg
-import ParallelStencil: SUPPORTED_PACKAGES, PKG_CUDA
+import ParallelStencil: SUPPORTED_PACKAGES, PKG_CUDA, PKG_AMDGPU
 TEST_PACKAGES = SUPPORTED_PACKAGES
 @static if PKG_CUDA in TEST_PACKAGES
     import CUDA
     if !CUDA.functional() TEST_PACKAGES = filter!(x->x≠PKG_CUDA, TEST_PACKAGES) end
+end
+@static if PKG_AMDGPU in TEST_PACKAGES
+    import AMDGPU
+    if !AMDGPU.functional() TEST_PACKAGES = filter!(x->x≠PKG_AMDGPU, TEST_PACKAGES) end
 end
 
 @static for package in TEST_PACKAGES  eval(:(
