0286434844189bcd5da7db42c26211ccd36fdd66
Samuel Omlin <samuel.omlin@cscs.ch>
2022-12-20 15:44:38 +0100

    introduce stencilsize

--- src/kernel_language.jl
+++ src/kernel_language.jl
@@ -52,33 +52,34 @@ function loop(index::Symbol, optdim::Integer, loopsize, body; package::Symbol=ge
 end
 
 
-#function add_loopopt(body::Expr, indices::Array{<:Union{Expr,Symbol}}, optvars::Union{Expr,Symbol}, optdim::Integer, loopsize::Union{Expr,Symbol,Integer}, halosize::Union{Integer,NTuple{N,Integer} where N})
+#function add_loopopt(body::Expr, indices::Array{<:Union{Expr,Symbol}}, optvars::Union{Expr,Symbol}, optdim::Integer, loopsize::Union{Expr,Symbol,Integer}, stencilsize::Union{Integer,NTuple{N,Integer} where N})
 #TODO: see what to do with global consts as SHMEM_HALO_X,... Support later multiple vars for opt (now just A=T...)
 #TODO: add input check and errors
 #TODO: maybe gensym with macro @gensym
 # TODO: create a run time check for requirement: 
 # In order to be able to read the data into shared memory in only two statements, the number of threats must be at least half of the size of the shared memory block plus halo; thus, the total number of threads in each dimension must equal the range length, as else there would be smaller thread blocks at the boundaries (threads overlapping the range are sent home). These smaller blocks would be likely not to match the criteria for a correct reading of the data to shared memory. In summary the following requirements must be matched: @gridDim().x*@blockDim().x - $rangelength_x == 0; @gridDim().y*@blockDim().y - $rangelength_y > 0
-function loopopt(caller::Module, indices, optvars, optdim::Integer, loopsize, halosize, indices_shift, body; package::Symbol=get_package())
+function loopopt(caller::Module, indices, optvars, optdim::Integer, loopsize, stencilsize, indices_shift, body; package::Symbol=get_package())
     if !isa(optvars, Symbol) @KeywordArgumentError("at present, only one optvar is supported.") end
     A = optvars 
     if (package ∉ SUPPORTED_PACKAGES) @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).") end
     if isa(indices,Expr) indices = indices.args else indices = (indices,) end
-    halosize = eval_arg(caller, halosize)
+    stencilsize = eval_arg(caller, stencilsize)
     noexpr = :(begin end)
     if optdim == 3
         ranges         = RANGES_VARNAME
         rangelength_z  = RANGELENGTHS_VARNAMES[3]
         tz_g           = THREADIDS_VARNAMES[3]
-        hx, hy         = halosize
-        shmem          = (hx>0 || hy>0)
+        sx1,sx2, sy1,sy2 ,sz1,sz2 = stencilsize
+        hx1,hx2, hy1,hy2 = stencilsize
+        shmem          = (hx1>0 || hx2>0 || hy1>0 || hy2>0)
         _ix, _iy, _iz  = indices
         _i             = gensym_world("i", @__MODULE__)
-        ix             = (indices_shift[1] > 0) ? :(($_ix + $(indices_shift[1]))) : _ix
-        iy             = (indices_shift[2] > 0) ? :(($_iy + $(indices_shift[2]))) : _iy
-        iz             = (indices_shift[3] > 0) ? :(($_iz + $(indices_shift[3]))) : _iz
-        i              = (indices_shift[3] > 0) ? :(($_i  - $(indices_shift[3]))) : _i
-        range_z        = (indices_shift[3] > 0) ? :(($ranges[3])[$tz_g] - $(indices_shift[3])) : :(($ranges[3])[$tz_g])
-        range_z_start  = (indices_shift[3] > 0) ? :(($ranges[3])[1]     - $(indices_shift[3])) : :(($ranges[3])[1])
+        ix             = _ix # (indices_shift[1] > 0) ? :(($_ix + $(indices_shift[1]))) : _ix
+        iy             = _iy # (indices_shift[2] > 0) ? :(($_iy + $(indices_shift[2]))) : _iy
+        iz             = _iz # (indices_shift[3] > 0) ? :(($_iz + $(indices_shift[3]))) : _iz
+        i              = _i # (indices_shift[3] > 0) ? :(($_i  - $(indices_shift[3]))) : _i
+        range_z        = :(($ranges[3])[$tz_g]) # (indices_shift[3] > 0) ? :(($ranges[3])[$tz_g] - $(indices_shift[3])) : :(($ranges[3])[$tz_g])
+        range_z_start  = :(($ranges[3])[1])     # (indices_shift[3] > 0) ? :(($ranges[3])[1]     - $(indices_shift[3])) : :(($ranges[3])[1])
         tx             = gensym_world("tx", @__MODULE__)
         ty             = gensym_world("ty", @__MODULE__)
         nx_l           = gensym_world("nx_l", @__MODULE__)
@@ -103,17 +104,17 @@ function loopopt(caller::Module, indices, optvars, optdim::Integer, loopsize, ha
         A_ix_iym1_iz   = gensym_world(string(A, "_ix_iym1_iz"), @__MODULE__)
         A_ix_iyp1_iz   = gensym_world(string(A, "_ix_iyp1_iz"), @__MODULE__)
 
-        body = substitute(body, :($A[$ix,$iy,$iz-1]), A_ix_iy_izm1)
+        if (sz1 > 0) body = substitute(body, :($A[$ix,$iy,$iz-1]), A_ix_iy_izm1) end
         body = substitute(body, :($A[$ix,$iy,$iz  ]), A_ix_iy_iz  )
-        body = substitute(body, :($A[$ix,$iy,$iz+1]), A_ix_iy_izp1)
-        if hx > 0
-            body = substitute(body, :($A[$ix-1,$iy,$iz]), A_ixm1_iy_iz)
-            body = substitute(body, :($A[$ix+1,$iy,$iz]), A_ixp1_iy_iz)
-        end
-        if hy > 0
-            body = substitute(body, :($A[$ix,$iy-1,$iz]), A_ix_iym1_iz)
-            body = substitute(body, :($A[$ix,$iy+1,$iz]), A_ix_iyp1_iz)
-        end
+        if (sz2 > 0) body = substitute(body, :($A[$ix,$iy,$iz+1]), A_ix_iy_izp1) end
+        # if hx > 0
+            if (sx1 > 0) body = substitute(body, :($A[$ix-1,$iy,$iz]), A_ixm1_iy_iz) end
+            if (sx2 > 0) body = substitute(body, :($A[$ix+1,$iy,$iz]), A_ixp1_iy_iz) end
+        # end
+        # if hy > 0
+            if (sy1 > 0) body = substitute(body, :($A[$ix,$iy-1,$iz]), A_ix_iym1_iz) end
+            if (sy1 > 0) body = substitute(body, :($A[$ix,$iy+1,$iz]), A_ix_iyp1_iz) end
+        # end
         if indices_shift[3] > 0
             body =  quote 
                         if ($i > 0)
@@ -127,32 +128,36 @@ function loopopt(caller::Module, indices, optvars, optdim::Integer, loopsize, ha
                         end
                     end
         end
-        loopstart = 0 #TODO: if in z neighbors beyond iz-1 are accessed, then this needs to be bigger (also range_z_start-...!). NOTE: not the same as halosize which is for shmemhalo
+        loopstart = 0 #TODO: if in z neighbors beyond iz-1 are accessed, then this needs to be bigger (also range_z_start-...!). NOTE: not the same as stencilsize which is for shmemhalo
 
         return quote
-                    $tx            = @threadIdx().x + $hx
-                    $ty            = @threadIdx().y + $hy
-                    $nx_l          = @blockDim().x + (2*$hx)
-                    $ny_l          = @blockDim().y + (2*$hy)
+                    $tx            = @threadIdx().x + $hx1
+                    $ty            = @threadIdx().y + $hy1
+$(shmem ? quote     
+                    $nx_l          = @blockDim().x + $(hx1+hx2)
+                    $ny_l          = @blockDim().y + $(hy1+hy2)
                     $t_h           = (@threadIdx().y-1)*@blockDim().x + @threadIdx().x # NOTE: here it must be bx, not @blockDim().x
                     $t_h2          = $t_h + $nx_l*$ny_l - @blockDim().x*@blockDim().y
                     $tx_h          = ($t_h-1) % $nx_l + 1
                     $ty_h          = ($t_h-1) ÷ $nx_l + 1
                     $tx_h2         = ($t_h2-1) % $nx_l + 1
                     $ty_h2         = ($t_h2-1) ÷ $nx_l + 1
-                    $ix_h          = (@blockIdx().x-1)*@blockDim().x + $tx_h  - $hx    # NOTE: here it must be @blockDim().x, not bx
-                    $ix_h2         = (@blockIdx().x-1)*@blockDim().x + $tx_h2 - $hx    # ...
-                    $iy_h          = (@blockIdx().y-1)*@blockDim().y + $ty_h  - $hy    # ...
-                    $iy_h2         = (@blockIdx().y-1)*@blockDim().y + $ty_h2 - $hy    # ...
+                    $ix_h          = (@blockIdx().x-1)*@blockDim().x + $tx_h  - $hx1    # NOTE: here it must be @blockDim().x, not bx
+                    $ix_h2         = (@blockIdx().x-1)*@blockDim().x + $tx_h2 - $hx1    # ...
+                    $iy_h          = (@blockIdx().y-1)*@blockDim().y + $ty_h  - $hy1    # ...
+                    $iy_h2         = (@blockIdx().y-1)*@blockDim().y + $ty_h2 - $hy1    # ...
+        end : 
+        noexpr
+)
                     $loopoffset    = (@blockIdx().z-1)*$loopsize #TODO: MOVE UP - see no perf change! interchange other lines!
 $(shmem ? :(        $A_izp1        = @sharedMem(eltype($A), ($nx_l, $ny_l))              ) : noexpr)
                     $A_ix_iy_izm1  = 0.0
                     $A_ix_iy_iz    = (@blockIdx().z>1) ? $A[$ix,$iy,$range_z_start-1+$loopoffset] : 0.0
                     $A_ix_iy_izp1  = 0.0
-$(hx>0 ?  :(        $A_ixm1_iy_iz  = 0.0                                                 ) : noexpr)
-$(hx>0 ?  :(        $A_ixp1_iy_iz  = 0.0                                                 ) : noexpr)
-$(hy>0 ?  :(        $A_ix_iym1_iz  = 0.0                                                 ) : noexpr)
-$(hy>0 ?  :(        $A_ix_iyp1_iz  = 0.0                                                 ) : noexpr)
+$(sx1>0 ?  :(       $A_ixm1_iy_iz  = 0.0                                                 ) : noexpr)
+$(sx2>0 ?  :(       $A_ixp1_iy_iz  = 0.0                                                 ) : noexpr)
+$(sy1>0 ?  :(       $A_ix_iym1_iz  = 0.0                                                 ) : noexpr)
+$(sy2>0 ?  :(       $A_ix_iyp1_iz  = 0.0                                                 ) : noexpr)
 
                     for $_i = $loopstart:$loopsize
                         $tz_g = $_i + $loopoffset
@@ -176,10 +181,10 @@ $(shmem ? :(            $A_ix_iy_izp1 = $A_izp1[$tx,$ty]
            )
 )
                         $body
-$(hx>0 ? :(             $A_ixm1_iy_iz = $A_izp1[$tx-1,$ty]                                ) : noexpr)
-$(hx>0 ? :(             $A_ixp1_iy_iz = $A_izp1[$tx+1,$ty]                                ) : noexpr)
-$(hy>0 ? :(             $A_ix_iym1_iz = $A_izp1[$tx,$ty-1]                                ) : noexpr)
-$(hy>0 ? :(             $A_ix_iyp1_iz = $A_izp1[$tx,$ty+1]                                ) : noexpr)
+$(sx1>0 ? :(            $A_ixm1_iy_iz = $A_izp1[$tx-1,$ty]                                ) : noexpr)
+$(sx2>0 ? :(            $A_ixp1_iy_iz = $A_izp1[$tx+1,$ty]                                ) : noexpr)
+$(sy1>0 ? :(            $A_ix_iym1_iz = $A_izp1[$tx,$ty-1]                                ) : noexpr)
+$(sy2>0 ? :(            $A_ix_iyp1_iz = $A_izp1[$tx,$ty+1]                                ) : noexpr)
                         $A_ix_iy_izm1 = $A_ix_iy_iz
                         $A_ix_iy_iz   = $A_ix_iy_izp1
                     end
@@ -190,17 +195,17 @@ $(hy>0 ? :(             $A_ix_iyp1_iz = $A_izp1[$tx,$ty+1]                      
 end
 
 
-function loopopt(caller::Module, indices, optvars, optdim::Integer, loopsize, halosize, body; package::Symbol=get_package())
+function loopopt(caller::Module, indices, optvars, optdim::Integer, loopsize, stencilsize, body; package::Symbol=get_package())
     indices_shift = (0,0,0)
-    return loopopt(caller, indices, optvars, optdim, loopsize, halosize, indices_shift, body; package=package)
+    return loopopt(caller, indices, optvars, optdim, loopsize, stencilsize, indices_shift, body; package=package)
 end
 
 
 function loopopt(caller::Module, indices, optvars, body; package::Symbol=get_package())
     optdim   = isa(indices,Expr) ? length(indices.args) : 1
     loopsize = LOOPSIZE
-    halosize = (optdim == 3) ? (1,1) : (optdim == 2) ? 1 : 0
-    return loopopt(caller, indices, optvars, optdim, loopsize, halosize, body; package=package)
+    stencilsize = (optdim == 3) ? (1,1) : (optdim == 2) ? 1 : 0
+    return loopopt(caller, indices, optvars, optdim, loopsize, stencilsize, body; package=package)
 end
 
 
