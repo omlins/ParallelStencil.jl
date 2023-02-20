e1cb5de351c069be9b8f9c9378ae674d3aa3af46
Samuel Omlin <samuel.omlin@cscs.ch>
2023-02-20 11:54:29 +0100

    add shared memory offset handling for CPU

--- src/ParallelKernel/kernel_language.jl
+++ src/ParallelKernel/kernel_language.jl
@@ -190,4 +190,4 @@ macro sync_threads_cpu() esc(:(begin end)) end
 
 macro sharedMem_cpu(T, dims) :(MArray{Tuple{$(esc(dims))...}, $(esc(T)), length($(esc(dims))), prod($(esc(dims)))}(undef)); end # Note: A macro is used instead of a function as a creating a type stable function is not really possible (dims can take any values and they become part of the MArray type...). MArray is not escaped in order not to have to import StaticArrays in the user code.
 
-macro sharedMem_cpu(T, dims, offset) :(ParallelStencil.ParallelKernel.@sharedMem_cpu($T, $dims))
+macro sharedMem_cpu(T, dims, offset) :(ParallelStencil.ParallelKernel.@sharedMem_cpu($T, $dims)) end
