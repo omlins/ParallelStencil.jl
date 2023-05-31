df8d227b875b7f99a3e706630251fd96b87dea33
Samuel Omlin <samuel.omlin@cscs.ch>
2023-05-31 12:41:02 +0200

    add documentation about number type ommission

--- src/init_parallel_stencil.jl
+++ src/init_parallel_stencil.jl
@@ -21,12 +21,13 @@
 
 """
     @init_parallel_stencil(package, numbertype, ndims)
+    @init_parallel_stencil(package=..., ndims=...)
 
 Initialize the package ParallelStencil, giving access to its main functionality. Creates a module `Data` in the module where `@init_parallel_stencil` is called from. The module `Data` contains the types as `Data.Number`, `Data.Array` and `Data.CellArray` (type `?Data` *after* calling `@init_parallel_stencil` to see the full description of the module).
 
 # Arguments
 - `package::Module`: the package used for parallelization (CUDA, AMDGPU or Threads).
-- `numbertype::DataType`: the type of numbers used by @zeros, @ones, @rand and @fill and in all array types of module `Data` (e.g. Float32 or Float64). It is contained in `Data.Number` after @init_parallel_stencil.
+- `numbertype::DataType`: the type of numbers used by @zeros, @ones, @rand and @fill and in all array types of module `Data` (e.g. Float32 or Float64). It is contained in `Data.Number` after @init_parallel_stencil. The `numbertype` can be omitted if the other arguments are given as keyword arguments (in that case, the `numbertype` will have to be given explicitly when using the types provided by the module `Data`). 
 - `ndims::Integer`: the number of dimensions used for the stencil computations in the kernels (1, 2 or 3).
 
 See also: [`Data`](@ref)
