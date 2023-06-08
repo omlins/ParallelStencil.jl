69af3466bc3a706520cc9f5a48a109cddb2ae148
Samuel Omlin <samuel.omlin@cscs.ch>
2023-06-08 18:30:44 +0200

    fix incremental compilation unit tests

--- test/test_projects/Diffusion3D_CUDA/src/Diffusion3D_CUDA.jl
+++ test/test_projects/Diffusion3D_CUDA/src/Diffusion3D_CUDA.jl
@@ -3,4 +3,5 @@ module Diffusion3D_CUDA
     using ParallelStencil.FiniteDifferences3D
     @init_parallel_stencil(CUDA, Float64, 3)
     include(joinpath(@__DIR__, "..", "..", "shared", "diffusion3D.jl"))
+    ParallelStencil.@reset_parallel_stencil()
 end
