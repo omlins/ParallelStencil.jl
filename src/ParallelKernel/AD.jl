21797893bf031bc52539467293d07c0f1275b742
Samuel Omlin <samuel.omlin@cscs.ch>
2023-07-07 19:54:47 +0200

    add AD modules

---
+++ src/ParallelKernel/AD.jl
@@ -0,0 +1,29 @@
+"""
+Module AD
+
+Provides GPU-compatible functions for automatic differentiation. The functions rely on the Enzyme.jl package.
+
+# Usage
+    using ParallelKernel.AD
+
+# Functions
+- autodiff_deferred!
+- autodiff_deferred_thunk!
+
+To see a description of a function type `?<functionname>`.
+"""
+module AD
+export autodiff_deferred!, autodiff_deferred_thunk!
+import Enzyme
+
+function autodiff_deferred!(args...)
+    Enzyme.autodiff_deferred(args...)
+    return
+end
+
+function autodiff_deferred_thunk!(args...)
+    Enzyme.autodiff_deferred_thunk(args...)
+    return
+end
+
+end # Module AD
