56beca941b0eb671334b0230686dc0def49464bd
Samuel Omlin <samuel.omlin@cscs.ch>
2023-07-07 20:06:48 +0200

    fix documentation formatting

--- src/ParallelKernel/AD.jl
+++ src/ParallelKernel/AD.jl
@@ -7,8 +7,8 @@ Provides GPU-compatible functions for automatic differentiation. The functions r
     using ParallelKernel.AD
 
 # Functions
-- autodiff_deferred!
-- autodiff_deferred_thunk!
+- `autodiff_deferred!`
+- `autodiff_deferred_thunk!`
 
 To see a description of a function type `?<functionname>`.
 """
