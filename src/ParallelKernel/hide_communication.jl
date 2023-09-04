2d30b2147f4af209161c390747733bcab284c7c0
Samuel Omlin <samuel.omlin@cscs.ch>
2023-09-04 12:43:26 +0200

    enable multiple computation calls in hide communication

--- src/ParallelKernel/hide_communication.jl
+++ src/ParallelKernel/hide_communication.jl
@@ -1,6 +1,7 @@
-const ERRMSG_HIDE_COMMUNICATION = "@hide_communication: the code block must start with exactly one @parallel computation call and be followed by code to set boundary conditions and to perform communication. Type `?@hide_communication` for more information."
+const ERRMSG_HIDE_COMMUNICATION = "@hide_communication: the code block must normally start with one @parallel computation call (for exceptions, see keyword `computation_calls`) and be followed by code to set boundary conditions and to perform communication. Type `?@hide_communication` for more information."
 const ERRMSG_INVALID_STREAM = "@hide_communication: invalid keyword argument in @parallel <kernelcall>: stream. Specifying the stream is not allowed in a @parallel call inside the code block argument of @hide_communication."
-const ERRMSG_INVALID_COMP = "@hide_communication: invalid positional arguments in @parallel <kernelcall>: no positional argument is allowed in the @parallel call to perform computations inside the code block argument of @hide_communication."
+const ERRMSG_INVALID_COMP = "@hide_communication: invalid positional arguments in @parallel <kernelcall>: zero or one positional argument (ranges) is allowed in the @parallel call to perform computations inside the code block argument of @hide_communication <boundary_width> <block>."
+const ERRMSG_INVALID_COMP_2 = "@hide_communication: invalid positional arguments in @parallel <kernelcall>: no positional argument is allowed in the @parallel call to perform computations inside the code block argument of @hide_communication <ranges_outer> <ranges_inner> <block>."
 const ERRMSG_INVALID_BC_COMM = "@hide_communication: invalid positional arguments in @parallel <kernelcall>: exactly one positional argument argument (ranges) must be passed in @parallel calls to set boundary conditions and to perform communication inside the code block argument of @hide_communication."
 
 ##
@@ -8,17 +9,22 @@ const HIDE_COMMUNICATION_DOC = """
     @hide_communication boundary_width block
 
 !!! note "Advanced"
+        @hide_communication boundary_width block computation_calls=...
         @hide_communication ranges_outer ranges_inner block
+        @hide_communication ranges_outer ranges_inner block computation_calls=...
 
 Hide the communication behind the computation within the code `block`.
 
 # Arguments
 - `boundary_width::Tuple{Integer,Integer,Integer} | Tuple{Integer,Integer} | Tuple{Integer}`: width of the boundaries in each dimension. The boundaries must include (at least) all the data that is accessed in the communcation performed.
-- `block`: code block wich starts with exactly one [`@parallel`](@ref) call to perform computations, followed by code to set boundary conditions and to perform communication (as e.g. `update_halo!` from the package `ImplicitGlobalGrid`). The [`@parallel`](@ref) call to perform computations cannot contain any positional arguments (ranges, nblocks or nthreads) nor the stream keyword argument (stream=...). The code to set boundary conditions and to perform communication must only access the elements in the boundary ranges of the fields modified in the [`@parallel`](@ref) call; all elements can be acccessed from other fields. Moreover, this code must not include statements in array broadcasting notation, because they are always run on the default stream in CUDA (for CUDA.jl < v2.0), which makes CUDA stream overlapping impossible. Instead, boundary region elements can, e.g., be accessed with [`@parallel`](@ref) calls passing a ranges argument that ensures that no threads mapping to elements outside of `ranges_outer` are launched. Note that these [`@parallel`](@ref) `ranges` calls cannot contain any other positional arguments (nblocks or nthreads) nor the stream keyword argument (stream=...).
+- `block`: code block wich starts with one [`@parallel`](@ref) call to perform computations (for exceptions, see keyword `computation_calls`), followed by code to set boundary conditions and to perform communication (as e.g. `update_halo!` from the package `ImplicitGlobalGrid`). The [`@parallel`](@ref) call to perform computations can normally not contain any positional arguments (ranges, nblocks or nthreads) nor the stream keyword argument (stream=...) (for exceptions, see keyword `computation_calls`). The code to set boundary conditions and to perform communication must only access the elements in the boundary ranges of the fields modified in the [`@parallel`](@ref) call; all elements can be acccessed from other fields. Moreover, this code must not include statements in array broadcasting notation, because they are always run on the default stream in CUDA (for CUDA.jl < v2.0), which makes CUDA stream overlapping impossible. Instead, boundary region elements can, e.g., be accessed with [`@parallel`](@ref) calls passing a ranges argument that ensures that no threads mapping to elements outside of the boundary regions are launched. Note that these [`@parallel`](@ref) `ranges` calls cannot contain any other positional arguments (nblocks or nthreads) nor the stream keyword argument (stream=...).
 
 !!! note "Advanced"
     - `ranges_outer::`Tuple with one or multiple `ranges` as required by the corresponding argument of [`@parallel`](@ref): the `ranges` must together span (at least) all the data that is accessed in the communcation and boundary conditions performed.
     - `ranges_inner::`Tuple with one or multiple `ranges` as required by the corresponding argument of [`@parallel`](@ref): the `ranges` must together span the data that is not included by `ranges_outer`.
+
+!!! note "Advanced keyword arguments"
+    - `computation_calls=1::`number of [`@parallel`](@ref) calls at the start of the code `block` that contain the computation to be hidden behind communication. Only set this argument if you are sure that it is safe to deviate from the standard behavior (`computation_calls=1`). Note that, in many scenarios (slightly) wrong results will be obtained when `computation_calls` in greater than `1`; it is only safe, generally speaking, if no point in the boundary region computations depends on any inner point computation (finite difference derivatives can, for example, constitute such a dependency leading to wrong results).
 
 # Examples
     @hide_communication (16, 2, 2) begin
@@ -59,13 +65,30 @@ Hide the communication behind the computation within the code `block`.
         update_halo!(Te2);
     end
 
+    @hide_communication (16, 2, 2) begin
+        @parallel (1:size(Te,1), 1:size(Te,2), 1:size(Te,3)) diffusion3D_step!(Te2, Te, Ci, lam, dt, dx, dy, dz);
+        @parallel (1:size(Te,2), 1:size(Te,3)) bc_x(Te);
+        @parallel (1:size(Te,1), 1:size(Te,3)) bc_y(Te);
+        @parallel (1:size(Te,1), 1:size(Te,2)) bc_z(Te);
+        update_halo!(Te2);
+    end
+
+    @hide_communication (16, 2, 2) computation_calls=2 begin
+        @parallel computation1!(A2, A, B);
+        @parallel computation2!(B, C);
+        @parallel (1:size(A,2), 1:size(A,3)) bc_x(A);
+        @parallel (1:size(A,1), 1:size(A,3)) bc_y(A);
+        @parallel (1:size(A,1), 1:size(A,2)) bc_z(A);
+        update_halo!(Te2);
+    end
+
 !!! note "Developers note"
     The communcation should not perform any blocking operations to enable a maximal overlap of communication with computation.
 
 See also: [`@parallel`](@ref)
 """
 @doc HIDE_COMMUNICATION_DOC
-macro hide_communication(args...) check_initialized(); checkargs_hide_communication(args...); esc(hide_communication(args...)); end
+macro hide_communication(args...) check_initialized(); checkargs_hide_communication(args...); esc(hide_communication(__module__, args...)); end
 
 macro get_priority_stream(args...) check_initialized(); checkargs_get_stream(args...); esc(get_priority_stream(args...)); end
 macro get_stream(args...) check_initialized(); checkargs_get_stream(args...); esc(get_stream(args...)); end
@@ -74,7 +97,8 @@ macro get_stream(args...) check_initialized(); checkargs_get_stream(args...); es
 ## ARGUMENT CHECKS
 
 function checkargs_hide_communication(args...)
-    if (length(args) < 2 || length(args) > 3) @ArgumentError("wrong number of arguments.") end
+    posargs, ~ = split_args(args)
+    if (length(posargs) < 2 || length(posargs) > 3) @ArgumentError("wrong number of positional arguments.") end
     if !is_block(args[end]) @ArgumentError("the last argument must be a code block (obtained: $(args[end])).") end
 end
 
@@ -85,9 +109,11 @@ end
 
 ## GATEWAY FUNCTIONS
 
-function hide_communication(args::Union{Integer,Symbol,Expr}...; package::Symbol=get_package())
-    if     isgpu(package)           hide_communication_gpu(args...)
-    elseif (package == PKG_THREADS) hide_communication_threads(args...)
+function hide_communication(caller::Module, args::Union{Integer,Symbol,Expr}...; package::Symbol=get_package())
+    posargs, kwargs_expr = split_args(args)
+    kwargs, ~ = extract_kwargs(caller, kwargs_expr, (:computation_calls,), "@hide_communication", false; eval_args=(:computation_calls,))
+    if     isgpu(package)           hide_communication_gpu(posargs...; kwargs...)
+    elseif (package == PKG_THREADS) hide_communication_threads(posargs...; kwargs...)
     else                            @KeywordArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package).")
     end
 end
@@ -108,19 +134,26 @@ end
 
 ## @HIDE_COMMUNICATION FUNCTIONS
 
-function hide_communication_gpu(ranges_outer::Union{Symbol,Expr}, ranges_inner::Union{Symbol,Expr}, block::Expr)
-    compcall, bc_and_commcalls = extract_calls(block)
-    parallel_args = extract_args(compcall, Symbol("@parallel"))
-    posargs, kwargs, compkernelcall = split_parallel_args(parallel_args)
-    if any([x.args[1]==:stream for x in kwargs]) @ArgumentError(ERRMSG_INVALID_STREAM) end
-    if (length(posargs) > 0) @ArgumentError(ERRMSG_INVALID_COMP) end
+function hide_communication_gpu(ranges_outer::Union{Symbol,Expr}, ranges_inner::Union{Symbol,Expr}, block::Expr; computation_calls::Integer=1)
+    if (computation_calls < 1) @KeywordArgumentError("Invalid keyword argument in @hide_communication: computation_calls must be >= 1.") end
+    compcalls, bc_and_commcalls = extract_calls(block, computation_calls)
+    compcalls_outer = []
+    compcalls_inner = []
+    for compcall in compcalls
+        parallel_args = extract_args(compcall, Symbol("@parallel"))
+        posargs, kwargs, compkernelcall = split_parallel_args(parallel_args)
+        if any([x.args[1]==:stream for x in kwargs]) @ArgumentError(ERRMSG_INVALID_STREAM) end
+        if (length(posargs) > 0) @ArgumentError(ERRMSG_INVALID_COMP_2) end
+        push!(compcalls_outer, :(@parallel_async $ranges_outer[i] stream=ParallelStencil.ParallelKernel.@get_priority_stream(i) $(kwargs...) $compkernelcall)) #NOTE: it cannot directly go to ParallelStencil.ParallelKernel.@parallel_async as else it cannot honour ParallelStencil args as memopt (fixing it to ParallelStencil is also not possible as it assumes, else the ParalellKernel hide_communication unit tests fail).
+        push!(compcalls_inner, :(@parallel_async $ranges_inner[i] stream=ParallelStencil.ParallelKernel.@get_stream(i) $(kwargs...) $compkernelcall))          #NOTE: ...
+    end
     bc_and_commcalls = process_bc_and_commcalls(bc_and_commcalls)
     quote
         for i in 1:length($ranges_outer)
-            @parallel_async $ranges_outer[i] stream=ParallelStencil.ParallelKernel.@get_priority_stream(i) $(kwargs...) $compkernelcall #NOTE: it cannot directly go to ParallelStencil.ParallelKernel.@parallel_async as else it cannot honour ParallelStencil args as memopt (fixing it to ParallelStencil is also not possible as it assumes, else the ParalellKernel hide_communication unit tests fail).
+            $(compcalls_outer...)
         end
         for i in 1:length($ranges_inner)
-            @parallel_async $ranges_inner[i] stream=ParallelStencil.ParallelKernel.@get_stream(i) $(kwargs...) $compkernelcall          #NOTE: ...
+            $(compcalls_inner...)
         end
         for i in 1:length($ranges_outer) ParallelStencil.ParallelKernel.@synchronize(ParallelStencil.ParallelKernel.@get_priority_stream(i)); end
         $bc_and_commcalls
@@ -128,28 +161,37 @@ function hide_communication_gpu(ranges_outer::Union{Symbol,Expr}, ranges_inner::
     end
 end
 
-function hide_communication_threads(ranges_outer::Union{Symbol,Expr}, ranges_inner::Union{Symbol,Expr}, block::Expr)
+function hide_communication_threads(ranges_outer::Union{Symbol,Expr}, ranges_inner::Union{Symbol,Expr}, block::Expr; computation_calls::Integer=1)
     return block #NOTE: This is currently not implemented, but enables correct execution nevertheless.
 end
 
 
-function hide_communication_gpu(boundary_width::Union{Integer,Symbol,Expr}, block::Expr)
-    compcall, = extract_calls(block)
-    parallel_args = extract_args(compcall, Symbol("@parallel"))
-    posargs, kwargs, compkernelcall = split_parallel_args(parallel_args)
-    if any([x.args[1]==:stream for x in kwargs]) @ArgumentError(ERRMSG_INVALID_STREAM) end
-    if (length(posargs) > 0) @ArgumentError(ERRMSG_INVALID_COMP) end
-    ranges = :(ParallelStencil.ParallelKernel.get_ranges($(compkernelcall.args[2:end]...)))
+function hide_communication_gpu(boundary_width::Union{Integer,Symbol,Expr}, block::Expr; computation_calls::Integer=1)
+    if (computation_calls < 1) @KeywordArgumentError("Invalid keyword argument in @hide_communication: computation_calls must be >= 1.") end
+    compcalls, bc_and_commcalls = extract_calls(block, computation_calls)
+    compranges = []
+    for i in 1:length(compcalls)
+        parallel_args = extract_args(compcalls[i], Symbol("@parallel"))
+        posargs, kwargs, compkernelcall = split_parallel_args(parallel_args)
+        if any([x.args[1]==:stream for x in kwargs]) @ArgumentError(ERRMSG_INVALID_STREAM) end
+        if (length(posargs) > 1) @ArgumentError(ERRMSG_INVALID_COMP) end
+        ranges = (length(posargs) == 1) ? posargs[1] : :(ParallelStencil.ParallelKernel.get_ranges($(compkernelcall.args[2:end]...)))
+        push!(compranges, ranges)
+        compcalls[i] = :(@parallel $(kwargs...) $compkernelcall)
+    end
     ranges_outer = gensym_world("ranges_outer", @__MODULE__)
     ranges_inner = gensym_world("ranges_inner", @__MODULE__)
     quote
-        $ranges_outer = ParallelStencil.ParallelKernel.get_ranges_outer($boundary_width, $ranges)
-        $ranges_inner = ParallelStencil.ParallelKernel.get_ranges_inner($boundary_width, $ranges)
-        ParallelStencil.ParallelKernel.@hide_communication $ranges_outer $ranges_inner $block  # NOTE: to enable macros like @hide_communication_cuda, one would need to use everywhere in hide_communication_cuda _cuda and _thread suffixed macros and enable their processing. This is more complex and generates less readable code though. => Design decision: only enable _cuda/_threads macros for macros that do not return package dependent macros.
+        $ranges_outer = ParallelStencil.ParallelKernel.get_ranges_outer($boundary_width, $(compranges...))
+        $ranges_inner = ParallelStencil.ParallelKernel.get_ranges_inner($boundary_width, $(compranges...))
+        ParallelStencil.ParallelKernel.@hide_communication $ranges_outer $ranges_inner computation_calls=$computation_calls begin  # NOTE: to enable macros like @hide_communication_cuda, one would need to use everywhere in hide_communication_cuda _cuda and _thread suffixed macros and enable their processing. This is more complex and generates less readable code though. => Design decision: only enable _cuda/_threads macros for macros that do not return package dependent macros.
+            $(compcalls...)
+            $(unblock(bc_and_commcalls))
+        end
     end
 end
 
-function hide_communication_threads(boundary_width::Union{Integer,Symbol,Expr}, block::Expr)
+function hide_communication_threads(boundary_width::Union{Integer,Symbol,Expr}, block::Expr; computation_calls::Integer=1)
     return block #NOTE: This is currently not implemented, but enables correct execution nevertheless.
 end
 
@@ -164,12 +206,13 @@ get_stream_amdgpu(id::Union{Integer,Symbol,Expr})          = return :(ParallelSt
 
 ## FUNCTIONS TO EXTRACT AND PROCESS COMPUTATION AND BOUNDARY CONDITIONS CALLS / COMMUNICATION CALLS
 
-function extract_calls(block::Expr)
-    if (sum([isa(arg, Expr) for arg in block.args]) < 2) @ArgumentError(ERRMSG_HIDE_COMMUNICATION) end
-    compcall         = block.args[2]
-    bc_and_commcalls = quote $(block.args[3:end]...) end
-    if !is_parallel_call(compcall) @ArgumentError(ERRMSG_HIDE_COMMUNICATION) end
-    return compcall, bc_and_commcalls
+function extract_calls(block::Expr, computation_calls::Integer)
+    if (sum([isa(arg, Expr) for arg in block.args]) < 1+computation_calls) @ArgumentError(ERRMSG_HIDE_COMMUNICATION) end
+    statements       = filter(x->!isa(x, LineNumberNode), block.args)
+    compcalls        = statements[1:computation_calls]
+    bc_and_commcalls = quote $(statements[computation_calls+1:end]...) end
+    if !all(is_parallel_call.(compcalls)) @ArgumentError(ERRMSG_HIDE_COMMUNICATION) end
+    return compcalls, bc_and_commcalls
 end
 
 function process_bc_and_commcalls(block::Expr)
@@ -192,7 +235,12 @@ promote_boundary_width(boundary_width::BOUNDARY_WIDTH_TYPE_2D)       = (boundary
 promote_boundary_width(boundary_width::BOUNDARY_WIDTH_TYPE)          = boundary_width
 promote_boundary_width(boundary_width)                               = @ArgumentError("@hide_communication: boundary_width must be a Tuple of Integer of size 1, 2 or 3 (obtained: $boundary_width; its type is: $(typeof(boundary_width))).")
 
-function get_ranges_outer(boundary_width, ranges::RANGES_TYPE)
+function get_ranges_outer(boundary_width, ranges...::RANGES_TYPE)
+    if length(ranges) > 1
+        ranges = ranges[1]
+    else
+        ranges = ranges[1]
+    end
     boundary_width = promote_boundary_width(boundary_width)
     validate_ranges_args(boundary_width, ranges)
     ms = length.(ranges)
