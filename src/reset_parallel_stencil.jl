2b7be8a93b04c549d48a47a380bd3fe6f3d44656
Samuel Omlin <samuel.omlin@cscs.ch>
2023-12-18 15:34:55 +0100

    move to per module PS parameters

--- src/reset_parallel_stencil.jl
+++ src/reset_parallel_stencil.jl
@@ -9,9 +9,9 @@ macro reset_parallel_stencil() esc(reset_parallel_stencil(__module__)) end
 
 function reset_parallel_stencil(caller::Module)
     ParallelKernel.reset_parallel_kernel(caller)
-    set_initialized(false)
-    set_package(PKG_NONE)
-    set_numbertype(NUMBERTYPE_NONE)
-    set_ndims(NDIMS_NONE)
+    set_initialized(caller, false)
+    set_package(caller, PKG_NONE)
+    set_numbertype(caller, NUMBERTYPE_NONE)
+    set_ndims(caller, NDIMS_NONE)
     return nothing
 end
