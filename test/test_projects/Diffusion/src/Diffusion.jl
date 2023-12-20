be7d79baf7165c732cd3fdc931c7a13e7472f448
Samuel Omlin <samuel.omlin@cscs.ch>
2023-12-20 18:02:36 +0100

    update test project diffusion

--- test/test_projects/Diffusion/src/Diffusion.jl
+++ test/test_projects/Diffusion/src/Diffusion.jl
@@ -1,5 +1,5 @@
 module Diffusion
-    export CPUBackend, CUDABackend, AMDGPUBackend, diffusion2D, diffusion3D
+    export CPUBackend, CUDABackend, AMDGPUBackend, diffusion2D, diffusion3D, memcopy!
 
     abstract type AbstractBackend end
     struct CPUBackend <: AbstractBackend end
@@ -8,6 +8,7 @@ module Diffusion
 
     diffusion2D(backend::Type{<:AbstractBackend}) = error("module not loaded or diffusion2D not implemented for backend: $backend")
     diffusion3D(backend::Type{<:AbstractBackend}, NumberType) = error("module not loaded or diffusion3D not implemented for backend: $backend")
+    memcopy!(B, A) = error("module not loaded or memcopy! not implemented for types: $(typeof(A)) and $(typeof(B))") 
 
     include("CPU.jl")
 end
