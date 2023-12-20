82a10cfcf44a88b6bd32d768ceb0df5b236288e2
Samuel Omlin <samuel.omlin@cscs.ch>
2023-12-20 18:47:57 +0100

    activate minimal extension test project

--- test/test_extensions.jl
+++ test/test_extensions.jl
@@ -10,7 +10,7 @@ end
     if !AMDGPU.functional() TEST_PACKAGES = filter!(x->x≠PKG_AMDGPU, TEST_PACKAGES) end
 end
 exename = joinpath(Sys.BINDIR, Base.julia_exename())
-const TEST_PROJECTS = ["Diffusion"] # ["Diffusion3D_minimal", "Diffusion3D", "Diffusion"]
+const TEST_PROJECTS = ["Diffusion3D_minimal"] # ["Diffusion3D_minimal", "Diffusion3D", "Diffusion"]
 
 @static for package in TEST_PACKAGES  eval(:(
     @testset "$(basename(@__FILE__)) (package: $(nameof($package)))" begin
