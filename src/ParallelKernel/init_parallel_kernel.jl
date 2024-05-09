a4f02f30d497279ed49b4c17f640ee9101578eb4
Samuel Omlin <samuel.omlin@cscs.ch>
2024-05-09 19:53:11 +0200

    update initialization for polyester

--- src/ParallelKernel/init_parallel_kernel.jl
+++ src/ParallelKernel/init_parallel_kernel.jl
@@ -28,17 +28,17 @@ end
 function init_parallel_kernel(caller::Module, package::Symbol, numbertype::DataType, inbounds::Bool; datadoc_call=:())
     modulename = :Data
     if package == PKG_CUDA
-        if (!is_installed("CUDA")) @NotInstalledError("CUDA was selected as package for parallelization, but CUDA.jl is not installed. CUDA functionality is provided with an extension of ParallelStencil and CUDA.jl needs therefore to be installed independently.") end
+        if (!is_installed("CUDA")) @NotInstalledError("CUDA was selected as package for parallelization, but CUDA.jl is not installed. CUDA functionality is provided as an extension of ParallelStencil and CUDA.jl needs therefore to be installed independently.") end
         indextype          = INT_CUDA
         data_module        = Data_cuda(modulename, numbertype, indextype)
         pkg_import_cmd     = :(import CUDA)
     elseif package == PKG_AMDGPU
-        if (!is_installed("AMDGPU")) @NotInstalledError("AMDGPU was selected as package for parallelization, but AMDGPU.jl is not installed. AMDGPU functionality is provided with an extension of ParallelStencil and AMDGPU.jl needs therefore to be installed independently.") end
+        if (!is_installed("AMDGPU")) @NotInstalledError("AMDGPU was selected as package for parallelization, but AMDGPU.jl is not installed. AMDGPU functionality is provided as an extension of ParallelStencil and AMDGPU.jl needs therefore to be installed independently.") end
         indextype          = INT_AMDGPU
         data_module        = Data_amdgpu(modulename, numbertype, indextype)
         pkg_import_cmd     = :(import AMDGPU)
     elseif package == PKG_POLYESTER
-        if (!is_installed("Polyester")) @NotInstalledError("Polyester was selected as package for parallelization, but Polyester.jl is not installed. Multi-threading using Polyester is provided with an extension of ParallelStencil and Polyester.jl needs therefore to be installed independently.") end
+        if (!is_installed("Polyester")) @NotInstalledError("Polyester was selected as package for parallelization, but Polyester.jl is not installed. Multi-threading using Polyester is provided as an extension of ParallelStencil and Polyester.jl needs therefore to be installed independently.") end
         indextype          = INT_POLYESTER
         data_module        = Data_cpu(modulename, numbertype, indextype)
         pkg_import_cmd     = :(import Polyester)
@@ -48,7 +48,7 @@ function init_parallel_kernel(caller::Module, package::Symbol, numbertype::DataT
         pkg_import_cmd     = :()
     end
     # TODO: before it was ParallelStencil.ParallelKernel.PKG_THREADS, which activated it all weight i think, which should not be
-    ad_init_cmd = :(ParallelStencil.ParallelKernel.AD.init_AD(package))
+    ad_init_cmd = :(ParallelStencil.ParallelKernel.AD.init_AD($package))
     if !isdefined(caller, :Data) || (@eval(caller, isa(Data, Module)) &&  length(symbols(caller, :Data)) == 1)  # Only if the module Data does not exist in the caller or is empty, create it.
         if (datadoc_call==:())
             if (numbertype == NUMBERTYPE_NONE) datadoc_call = :(@doc ParallelStencil.ParallelKernel.DATA_DOC_NUMBERTYPE_NONE Data) 
