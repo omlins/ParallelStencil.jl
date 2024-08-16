a15c064e525edc0cdcd21c8d0c3338b6ec41a4bd
Samuel Omlin <samuel.omlin@cscs.ch>
2024-08-16 19:09:16 +0200

    add exception for missing dependencies

--- src/ParallelKernel/init_parallel_kernel.jl
+++ src/ParallelKernel/init_parallel_kernel.jl
@@ -25,7 +25,7 @@ macro init_parallel_kernel(args...)
     esc(init_parallel_kernel(__module__, package, numbertype_val, inbounds_val))
 end
 
-function init_parallel_kernel(caller::Module, package::Symbol, numbertype::DataType, inbounds::Bool; datadoc_call=:(), parent_module::String=ParallelKernel)
+function init_parallel_kernel(caller::Module, package::Symbol, numbertype::DataType, inbounds::Bool; datadoc_call=:(), parent_module::String="ParallelKernel")
     modulename = :Data
     if package == PKG_CUDA
         if (isinteractive() && !is_installed("CUDA")) @NotInstalledError("CUDA was selected as package for parallelization, but CUDA.jl is not installed. CUDA functionality is provided as an extension of $parent_module and CUDA.jl needs therefore to be installed independently (type `add CUDA` in the julia package manager).") end
