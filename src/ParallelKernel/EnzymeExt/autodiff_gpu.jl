226a4e261667f54a928cb74c15d2b77a294a192f
Pascal Aellig <pascal.aellig@hotmail.com>
2024-10-08 11:30:30 +0200

    fix AD test

--- src/ParallelKernel/EnzymeExt/autodiff_gpu.jl
+++ src/ParallelKernel/EnzymeExt/autodiff_gpu.jl
@@ -2,11 +2,11 @@ import ParallelStencil
 import ParallelStencil: PKG_THREADS, PKG_POLYESTER
 import Enzyme
 
-function ParallelStencil.ParallelKernel.AD.init_AD(package::Symbol)
-    if iscpu(package)
-        Enzyme.API.runtimeActivity!(true) # NOTE: this is currently required for Enzyme to work correctly with threads
-    end
-end
+# function ParallelStencil.ParallelKernel.AD.init_AD(package::Symbol)
+#     if iscpu(package)
+#         Enzyme.API.runtimeActivity!(true) # NOTE: this is currently required for Enzyme to work correctly with threads
+#     end
+# end
 
 function ParallelStencil.ParallelKernel.AD.autodiff_deferred!(arg, args...) # NOTE: minimal specialization is used to avoid overwriting the default method
     Enzyme.autodiff_deferred(arg, args...)
