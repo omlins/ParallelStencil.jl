eafb0dc37d58c27f20f687913bfefc0b0d406897
GiackAloZ <giacomo.aloisi1998@gmail.com>
2024-10-08 09:10:11 +0200

    Add MetalExt and weak dependency

---
+++ ext/ParallelStencil_MetalExt.jl
@@ -0,0 +1,4 @@
+module ParallelStencil_MetalExt
+    # include(joinpath(@__DIR__, "..", "src", "ParallelKernel", "MetalExt", "shared.jl"))
+    # include(joinpath(@__DIR__, "..", "src", "ParallelKernel", "MetalExt", "allocators.jl"))
+end
