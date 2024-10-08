d98ab6fe7ccc89a4edb34f4f4ae787cf248634ad
GiackAloZ <giacomo.aloisi1998@gmail.com>
2024-10-08 13:43:22 +0200

    Add shared functions

---
+++ src/ParallelKernel/MetalExt/shared.jl
@@ -0,0 +1,32 @@
+import ParallelStencil
+import ParallelStencil.ParallelKernel: INT_METAL, rand_cpu, fill_cpu, construct_cell, check_datatype, rand_metal, fill_metal
+using ParallelStencil.ParallelKernel.Exceptions
+using Metal, CellArrays, StaticArrays
+import Metal.MTL
+
+## TODO add Metal backend for CellArray
+# @define_MetalCellArray
+
+## FUNCTIONS TO CHECK EXTENSIONS SUPPORT
+
+ParallelStencil.ParallelKernel.is_loaded(::Val{:ParallelStencil_MetalExt}) = true
+
+## FUNCTIONS TO GET CREATE AND MANAGE METAL QUEUES
+
+ParallelStencil.ParallelKernel.get_priority_metalqueue(arg...) = get_priority_metalqueue(arg...)
+ParallelStencil.ParallelKernel.get_metalqueue(arg...)          = get_metalqueue(arg...)
+let 
+    global get_priority_metalqueue, get_metalqueue
+    priority_metalqueues = Array{MTLCommandQueue}(undef, 0)
+    metalqueues          = Array{MTLCommandQueue}(undef, 0)
+
+    function get_priority_metalqueue(id::Integer)
+        while (id > length(priority_metalqueues)) push!(priority_metalqueues, MTL.MTLCommandQueue(device())) end # No priority setting available in Metal queues.
+        return priority_metalqueues[id]
+    end
+
+    function get_metalqueue(id::Integer)
+        while (id > length(metalqueues)) push!(metalqueues, MTLCommandQueue(MTLDevice.default_device())) end
+        return metalqueues[id]
+    end
+end
