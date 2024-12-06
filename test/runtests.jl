d61bffdac7a2e361c7775b21bcc838d268de8127
Samuel Omlin <samuel.omlin@cscs.ch>
2024-12-06 18:29:10 +0100

    move storage of state to caller

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ import ParallelStencil: SUPPORTED_PACKAGES, PKG_CUDA, PKG_AMDGPU, PKG_METAL
 @static if (PKG_AMDGPU in SUPPORTED_PACKAGES) import AMDGPU end
 @static if (PKG_METAL in SUPPORTED_PACKAGES && Sys.isapple()) import Metal end
 
-excludedfiles = [ "test_excluded.jl", "test_incremental_compilation.jl"]; # TODO: test_incremental_compilation has to be deactivated until Polyester support released
+excludedfiles = [ "test_excluded.jl", "test_incremental_compilation.jl", "test_revise.jl"]; # TODO: test_incremental_compilation has to be deactivated until Polyester support released
 
 function runtests()
     exename   = joinpath(Sys.BINDIR, Base.julia_exename())
