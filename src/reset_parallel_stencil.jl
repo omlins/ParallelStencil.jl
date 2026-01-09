114399478c4291e0f370693a6bb60916f9051998
Samuel Omlin <samuel.omlin@cscs.ch>
2026-01-09 10:37:42 +0100

    add ka backend

--- src/reset_parallel_stencil.jl
+++ src/reset_parallel_stencil.jl
@@ -15,5 +15,6 @@ function reset_parallel_stencil(caller::Module)
         set_numbertype(caller, NUMBERTYPE_NONE)
         set_ndims(caller, NDIMS_NONE)
     end
+    ParallelKernel.reset_runtime_hardware!(PKG_NONE)
     return nothing
 end
