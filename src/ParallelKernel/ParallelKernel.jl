03e62da3dfe18bf209ee0313e2255acd567d90af
Samuel Omlin <samuel.omlin@cscs.ch>
2026-01-09 10:35:42 +0100

    add ka backend

--- src/ParallelKernel/ParallelKernel.jl
+++ src/ParallelKernel/ParallelKernel.jl
@@ -72,6 +72,7 @@ include(joinpath("EnzymeExt", "AD.jl"))
 ## Alphabetical include of defaults for extensions
 include(joinpath("AMDGPUExt", "defaults.jl"))
 include(joinpath("CUDAExt", "defaults.jl"))
+include(joinpath("KernelAbstractionsExt", "defaults.jl"))
 include(joinpath("MetalExt", "defaults.jl"))
 
 ## Include of constant parameters, types and syntax sugar shared in ParallelKernel module only
@@ -92,7 +93,7 @@ include("FieldAllocators.jl")
 
 ## Exports
 export @init_parallel_kernel, @parallel, @hide_communication, @parallel_indices, @parallel_async, @synchronize, @zeros, @ones, @rand, @falses, @trues, @fill, @fill!, @CellType
-export select_hardware, current_hardware
+export current_hardware, select_hardware
 export @gridDim, @blockIdx, @blockDim, @threadIdx, @sync_threads, @sharedMem, @pk_show, @pk_println, @∀
 export @warpsize, @laneid, @active_mask, @shfl_sync, @shfl_up_sync, @shfl_down_sync, @shfl_xor_sync, @vote_any_sync, @vote_all_sync, @vote_ballot_sync
 export PKNumber
