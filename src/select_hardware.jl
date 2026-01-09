539f3b96e5f2422633350f6533d0e1c3edadd145
Samuel Omlin <samuel.omlin@cscs.ch>
2026-01-09 10:34:08 +0100

    add select hardware

--- src/select_hardware.jl
+++ src/select_hardware.jl
@@ -1,11 +1,11 @@
 ##
 @doc replace(ParallelKernel.SELECT_HARDWARE_DOC, "ParallelKernel" => "ParallelStencil"
 ) function select_hardware(hardware::Symbol)
-    ParallelKernel.select_hardware(hardware)
+    return ParallelKernel.select_hardware(hardware)
 end
 
 ##
 @doc replace(ParallelKernel.CURRENT_HARDWARE_DOC, "ParallelKernel" => "ParallelStencil"
 ) function current_hardware()
-    ParallelKernel.current_hardware()
+    return ParallelKernel.current_hardware()
 end
