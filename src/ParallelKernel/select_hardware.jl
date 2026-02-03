3ad15bb8826615b1e1f28fa0fec79378e247888f
Samuel Omlin <samuel.omlin@cscs.ch>
2026-02-03 19:33:20 +0100

    add select and current hardware and handle

--- src/ParallelKernel/select_hardware.jl
+++ src/ParallelKernel/select_hardware.jl
@@ -18,8 +18,26 @@ For workflow guidance refer to the [interactive prototyping runtime selection se
 
 See also: [`current_hardware`](@ref)
 """
+
+const SUPPORTED_HARDWARE = Dict(PKG_THREADS              => (hardware_default(PKG_THREADS),),
+                                PKG_POLYESTER            => (hardware_default(PKG_POLYESTER),),
+                                PKG_CUDA                 => (hardware_default(PKG_CUDA),),
+                                PKG_AMDGPU               => (hardware_default(PKG_AMDGPU),),
+                                PKG_METAL                => (hardware_default(PKG_METAL),),
+                                PKG_KERNELABSTRACTIONS   => (hardware_default(PKG_KERNELABSTRACTIONS), :gpu_cuda, :gpu_amd, :gpu_metal, :gpu_oneapi))
+
 @doc SELECT_HARDWARE_DOC
 function select_hardware(caller::Module, hardware::Symbol)
+    createmeta_PK(caller)
+    package = get_package(caller)
+    if (package == PKG_NONE) @ArgumentError("ParallelKernel has not been initialized in module $caller (missing @init_parallel_kernel call).") end
+    supported = get(SUPPORTED_HARDWARE, package, nothing)
+    if isnothing(supported) @ArgumentError("$ERRMSG_UNSUPPORTED_PACKAGE (obtained: $package). Supported packages are: $(join(SUPPORTED_PACKAGES, ", ")).") end
+    if !(hardware in supported)
+        @ArgumentError("unsupported hardware selection for package $package (obtained: $hardware). Supported hardware symbols are: $(join(supported, ", ")).")
+    end
+    set_hardware(caller, hardware)
+    return nothing
 end
 
 ##
@@ -37,4 +55,27 @@ See also: [`select_hardware`](@ref)
 """
 @doc CURRENT_HARDWARE_DOC
 function current_hardware(caller::Module)
+    return get_hardware(caller)
 end
+
+function handle(hardware::Symbol)
+    package = get_package(@__MODULE__)
+    if package == PKG_KERNELABSTRACTIONS
+        if hardware == :cpu
+            return KernelAbstractions.CPU()
+        elseif hardware == :gpu_cuda
+            return KernelAbstractions.CUDABackend()
+        elseif hardware == :gpu_amd
+            return KernelAbstractions.ROCBackend()
+        elseif hardware == :gpu_metal
+            return KernelAbstractions.MetalBackend()
+        elseif hardware == :gpu_oneapi
+            return KernelAbstractions.oneAPIBackend()
+        end
+        @ArgumentError("unsupported KernelAbstractions hardware symbol (obtained: $hardware).")
+    else
+        @ArgumentError("hardware handle translation is only supported for multi-architecture backends (obtained: $package).")
+    end
+end
+
+hardware_handle(caller::Module) = handle(current_hardware(caller))
