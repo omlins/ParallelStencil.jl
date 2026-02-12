a46e6d6fb2a24e25e6e0f1a6ab4b2dfded18af78
Samuel Omlin <samuel.omlin@cscs.ch>
2026-02-12 10:00:27 +0100

    refactor Metal package import logic in test_kernel_language.jl to simplify conditional checks

--- test/test_kernel_language.jl
+++ test/test_kernel_language.jl
@@ -18,14 +18,8 @@ end
     end
 end
 @static if PKG_METAL in TEST_PACKAGES
-    @static if Sys.isapple()
-        import Metal
-        if !Metal.functional()
-            TEST_PACKAGES = filter!(x -> x ≠ PKG_METAL, TEST_PACKAGES)
-        end
-    else
-        TEST_PACKAGES = filter!(x -> x ≠ PKG_METAL, TEST_PACKAGES)
-    end
+    import Metal
+    if !Metal.functional() TEST_PACKAGES = filter!(x->x≠PKG_METAL, TEST_PACKAGES) end
 end
 @static if PKG_KERNELABSTRACTIONS in TEST_PACKAGES
     import KernelAbstractions
