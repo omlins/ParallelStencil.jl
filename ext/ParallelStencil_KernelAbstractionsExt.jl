c89fa05d7ca22cd052494dc4694dfd4c272aa511
Samuel Omlin <samuel.omlin@cscs.ch>
2026-02-13 11:17:51 +0100

    include select_hardware.jl in ParallelStencil_KernelAbstractionsExt module for enhanced hardware selection

--- ext/ParallelStencil_KernelAbstractionsExt.jl
+++ ext/ParallelStencil_KernelAbstractionsExt.jl
@@ -1,4 +1,5 @@
 module ParallelStencil_KernelAbstractionsExt
     include(joinpath(@__DIR__, "..", "src", "ParallelKernel", "KernelAbstractionsExt", "shared.jl"))
+    include(joinpath(@__DIR__, "..", "src", "ParallelKernel", "KernelAbstractionsExt", "select_hardware.jl"))
     include(joinpath(@__DIR__, "..", "src", "ParallelKernel", "KernelAbstractionsExt", "allocators.jl"))
 end
