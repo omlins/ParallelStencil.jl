51faf648093ad1925e842a3b9ce50a5195cb0424
Samuel Omlin <samuel.omlin@cscs.ch>
2026-02-16 17:29:30 +0100

    refactor handle_kernelabstractions function to use specific backend handlers for CUDA, AMD, Metal, and OneAPI

--- src/ParallelKernel/KernelAbstractionsExt/select_hardware.jl
+++ src/ParallelKernel/KernelAbstractionsExt/select_hardware.jl
@@ -4,13 +4,13 @@ function ParallelStencil.ParallelKernel.handle_kernelabstractions(hardware::Symb
     if hardware == :cpu
         return KernelAbstractions.CPU()
     elseif hardware == :gpu_cuda
-        return KernelAbstractions.CUDABackend()
+        return ParallelStencil.ParallelKernel.handle_kernelabstractions_cuda()
     elseif hardware == :gpu_amd
-        return KernelAbstractions.ROCBackend()
+        return ParallelStencil.ParallelKernel.handle_kernelabstractions_amd()
     elseif hardware == :gpu_metal
-        return KernelAbstractions.MetalBackend()
+        return ParallelStencil.ParallelKernel.handle_kernelabstractions_metal()
     elseif hardware == :gpu_oneapi
-        return KernelAbstractions.oneAPIBackend()
+        return ParallelStencil.ParallelKernel.handle_kernelabstractions_oneapi()
     end
     @ArgumentError("unsupported KernelAbstractions hardware symbol (obtained: $hardware).")
 end
